6436ba7fd86bdc3f59573424db958d990a8271fd
Even Wei <shinycockorach@gmail.com>
2023-10-04 12:12:32 +0800

    Improve parse, get data from __NEXT_DATA__
    Signed-off-by: Even Wei <shinycockorach@gmail.com>

--- parser/parser.py
+++ parser/parser.py
@@ -1,106 +1,100 @@
-import os
-import re
 import json
 import requests
-from bs4 import BeautifulSoup
+import logging
+import tqdm  # Import the tqdm module for progress indication
 
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+logging.basicConfig(level=logging.ERROR)
+
+def fetch_webpage(url):
+    """Fetches the webpage content from the specified URL."""
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.RequestException as e:
+        logging.error(f"Failed to fetch the webpage: {e}")
+        return None
+
+def parse_data(html_text, key):
+    """Parses the HTML content to extract data based on a specified key."""
+    soup = BeautifulSoup(html_text, 'html.parser')
+    data_tag = soup.find('script', {'id': '__NEXT_DATA__'})
+    if data_tag is not None:
+        data = json.loads(data_tag.text)
+        result = data.get('props', {}).get('pageProps', {}).get(key)
+        if result is not None:
+            return result
+    return []
 
 def fetch_categories():
+    """Fetches the categories of data from the website."""
     url = 'https://www.moderndatastack.xyz/categories'
-    categories = []
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            cards = soup.find_all('a', id=re.compile('^categoryCard'))
-            for card in cards:
-                h5_tag = card.find('h5')
+    html_text = fetch_webpage(url)
+    if html_text is not None:
+        return parse_data(html_text, 'categories')
+    return []
 
-                id = card['href'].split('/')[-1] if card else 'No ID available'
-                name = h5_tag.text if h5_tag else 'No name available'
-                categories.append(dict(id=id, name=name))
-            return categories
-        else:
-            return f"Failed to fetch the webpage. Status code: {response.status_code}"
-    except Exception as e:
-        return f"An error occurred: {e}"
-
-
-def fetch_companies_by_category(category_id):
-    url = 'https://www.moderndatastack.xyz/companies/' + category_id
-    companies = []
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            cards = soup.select('div.card-body')
-            for card in cards:
-                a_tag = card.find('a')
-                h5_tag = card.find('h5')
-
-                id = a_tag['href'].split('/')[-1] if a_tag else 'No ID available'
-                name = h5_tag.text if h5_tag else 'No name available'
-                companies.append(dict(id=id, name=name, category_id=category_id))
-            return companies
-        else:
-            return f"Failed to fetch the webpage. Status code: {response.status_code}"
-    except Exception as e:
-        return f"An error occurred: {e}"
-
+def fetch_companies_by_category(category_slug):
+    """Fetches companies data by category from the website."""
+    url = f'https://www.moderndatastack.xyz/companies/{category_slug}'
+    html_text = fetch_webpage(url)
+    if html_text is not None:
+        companies = parse_data(html_text, 'companies')
+        for company in companies:
+            company['categorySlug'] = category_slug
+        return companies
+    return []
 
 def fetch_stacks():
+    """Fetches stacks of data from the website."""
     url = 'https://www.moderndatastack.xyz/stacks'
-    stacks = []
+    html_text = fetch_webpage(url)
+    if html_text is not None:
+        return parse_data(html_text, 'stacks')
+    return []
+
+def fetch_all_data():
+    """Fetches all the necessary data from the website."""
+    # Parse categories
+    categories = fetch_categories()
+
+    # Parse all companies in each category
+    vendors = []
+    for category in tqdm.tqdm(categories, desc="Fetching companies by category"):  # Add progress indication here
+        vendors.extend(fetch_companies_by_category(category['slug']))
+
+    # Parse data stacks
+    stacks = fetch_stacks()
+
+    return categories, vendors, stacks
+
+def save_to_json(data, filename):
+    """Saves the specified data to a JSON file."""
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            cards = soup.find_all('a', id=re.compile('^stackCard'))
-            for card in cards:
-                h4_tag = card.find('h4')
-                vendors = card.find_all('a')
-
-                id = card['href'].split('/')[-1] if card else 'No ID available'
-                name = h4_tag.text if h4_tag else 'No name available'
-                sub_stacks = []
-                for vendor in vendors:
-                    if not vendor['href'].startswith('/company'):
-                        continue
-                    vendor_id = vendor['href'].split('/')[-1] if vendor else 'No ID available'
-                    sub_stacks.append(dict(id=id, name=name, vendor_id=vendor_id))
-                stacks.extend(sub_stacks)
-            return stacks
-        else:
-            return f"Failed to fetch the webpage. Status code: {response.status_code}"
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+        print(f'Data saved to {filename}')
     except Exception as e:
-        return f"An error occurred: {e}"
+        print(f'An error occurred while saving the data: {e}')
 
+if __name__ == '__main__':
+    # Fetch all data from the website
+    categories, vendors, stacks = fetch_all_data()
 
-# # parse categories
-categories = fetch_categories()
+    # Create the JSON file path
+    current_script_dir = Path(__file__).resolve().parent
+    data_dir = current_script_dir.parent / 'raw_data'
+    data_dir.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
 
-# # parse all companies in each category
-vendors = []
-for category in categories:
-    vendors.extend(fetch_companies_by_category(category['id']))
+    # Define file paths
+    categories_file_path = data_dir / 'categories.json'
+    vendors_file_path = data_dir / 'vendors.json'
+    stacks_file_path = data_dir / 'stacks.json'
 
-# parse data stacks
-stacks = fetch_stacks()
-
-# Create the JSON file path
-current_script_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_script_dir)
-data_dir = os.path.join(parent_dir, 'raw_data')
-categories_file_path = os.path.join(data_dir, 'categories.json')
-vendors_file_path = os.path.join(data_dir, 'vendors.json')
-stacks_file_path = os.path.join(data_dir, 'stacks.json')
-
-# Write JSON files
-with open(categories_file_path, 'w') as json_file:
-    json.dump(categories, json_file, indent=4)
-
-with open(vendors_file_path, 'w') as json_file:
-    json.dump(vendors, json_file, indent=4)
-
-with open(stacks_file_path, 'w') as json_file:
-    json.dump(stacks, json_file, indent=4)
+    # Write JSON files
+    save_to_json(categories, categories_file_path)
+    save_to_json(vendors, vendors_file_path)
+    save_to_json(stacks, stacks_file_path)
